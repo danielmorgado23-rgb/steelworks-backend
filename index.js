88a632c1d6b743aa3ddca3542439bd8a23f77d65
Daniel Morgado <danielmorgado23@cbtis179.edu.mx>
2025-11-30 20:34:33 -0600

    Fix puerto para Render

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const app = express();
 // ==============================
 
 app.use(cors());
-app.use(express.json()); // MEJOR QUE body-parser
+app.use(express.json()); 
 app.use(express.static(path.join(__dirname, 'public')));
 
 // ==============================
@@ -174,11 +174,12 @@ app.get('/login.html', (req, res) => {
 // ==============================
 //  SERVIDOR
 // ==============================
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en el puerto ${PORT}`);
+  console.log(`Servidor corriendo en puerto ${PORT}`);
 });
+
 
 
 
